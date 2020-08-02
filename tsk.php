7d6a20f596748847af60ac613becee252383b2ff
Jophes7 <68357505+Jophes7@users.noreply.github.com>
2020-08-02 00:13:11 -0400

    up

--- tsk.php
+++ tsk.php
@@ -1,24 +1,10 @@
-<?php
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Trip to South Korea</title>
-	<meta charset="utf-8">
-	<meta name="viewport"
-	content="width=device-width, initial-scale=1.0">
-	<link href="TtSK.css" rel="stylesheet">
-<!--[if lt IE 9]>
-
-<![endif]-->
-</head>
+<html>
 <body>
-<div id="wrapper">
-	<header>
-	<div class="topnav">
-	<a href="index.html">
-	<img alt="Home" src="HomeLogo.png"></a></header>
-</div>
+
+<?php
+echo "My first PHP script!";
+?> 
+
 </body>
 </html>
-?>
